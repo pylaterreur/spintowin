2cd8788d98c9441e98f0a3eb2433cb7c8e4003c6
Pierre-Yves Le Borgne <pierre-yves.le-borgne@epitech.eu>
2013-06-21 07:31:19 +0800

    fake concept, can force the signature of a function in a class

---
+++ test/fake_concept.cpp
@@ -0,0 +1,44 @@
+
+#include <typeinfo>
+#include <iostream>
+
+struct Foo
+{
+  int f(char)
+  {
+    return (42);
+  }
+};
+
+template <typename Class>
+struct getUnderlyingFunctionType
+{
+  template <typename Ret, typename... Args>
+  static typename std::add_pointer<Ret(Args...)>::type caca(Ret (Class::*p)(Args...));
+};
+
+template <typename Implem>
+struct Concept : Implem
+{
+  using Implem::f;
+  static const char tab[-(int)
+			!std::is_same<
+			  typename std::remove_pointer<decltype(getUnderlyingFunctionType<Implem>::caca(&Implem::f))>::type,
+			  // BELOW IS THE SIGNATURE WE WANT TO FIND FOR f!
+			  int(char)
+			  >::value
+			];
+  enum { useless };
+};
+
+namespace {
+  struct Useless
+  {
+    // Foo doit respecter les regles definies dans Concept
+    enum { useless = Concept<Foo>::useless};
+  };
+}
+
+int main()
+{
+}
