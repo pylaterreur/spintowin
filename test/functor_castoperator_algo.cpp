6ddc9369263b052cfc4c85eb3c5540c2d8f9c468
Pierre-Yves Le Borgne <pierre-yves.leborgne@codecasesoftware.com>
2013-07-19 17:32:20 +0200

    added funny stuff using cast operator

---
+++ test/functor_castoperator_algo.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+
+#include <vector>
+#include <functional>
+#include <algorithm>
+
+namespace
+{
+  template <typename T>
+  struct increment
+  {
+    increment(T start) : counter_(start)
+    {}
+
+    T operator()()
+    {
+      return (++counter_);
+    }
+
+    operator int()
+    {
+      return (++counter_);
+    }
+
+  private:
+    T counter_;
+  };
+}
+
+int main()
+{
+  using namespace std::placeholders;
+
+  std::vector<int> v(10);
+  std::transform(v.begin(), v.end(), v.begin(), std::bind(std::plus<int>(), _1, increment<int>(0)));
+  std::for_each(v.begin(), v.end(), [](int i){ std::cout << i << std::endl; });
+}
