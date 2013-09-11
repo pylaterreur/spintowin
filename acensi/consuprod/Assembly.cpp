9b3fda53c54bcc0f51cd93d5674811f099473f26
Pierre-Yves Le Borgne <pierre-yves.le-borgne@cplusplusml.org>
2013-09-11 15:03:29 +0200

    layer combination works

--- acensi/consuprod/Assembly.cpp
+++ acensi/consuprod/Assembly.cpp
@@ -1,4 +1,41 @@
+#include <iostream>
 #include "Assembly.hpp"
 
+template <typename Super>
+struct B52 : Super
+{
+  struct Business : Super::Business
+  {
+    template <typename... T>
+    Business(T&&... t) : Super::Business(t...)
+    {
+      std::cout << "B52" << std::endl;
+    }
+  };
+};
+
+template <typename Super>
+struct A1 : Super
+{
+  struct Business : Super::Business
+  {
+    template <typename... T>
+    Business(T&&... t) : Super::Business(t...)
+    {
+      std::cout << "A1" << std::endl;
+    }
+  };
+};
+
+struct Foo
+{
+  Foo(const std::string &str)
+  {
+    std::cout << str << std::endl;
+  }
+};
+
 int main()
-{}
+{
+  Assembly<Foo, A1, B52>::Whole::Business f("abc");
+}
