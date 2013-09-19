90a6f3d940aaae96dc83d7a102aee772820c8a14
Pierre-Yves Le Borgne <pierre-yves.le-borgne@cplusplusml.org>
2013-09-19 16:01:57 +0200

    added the problem sentence

--- lvlup/templates/ex1.cpp
+++ lvlup/templates/ex1.cpp
@@ -9,5 +9,6 @@ struct Foo
 
 int main()
 {
+  // Write a class Bar that extracts a T from a C<T> type, and typedef it into an internal public Type
   assert((SameType<Bar<Foo<int> >::Type, int>::value));
 }
